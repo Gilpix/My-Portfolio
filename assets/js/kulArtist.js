4be55c66d7e56d47cf42aaa5ce52666b0f47a6ed
Gilpix <kuldeepsingh5005@gmail.com>
2020-05-28 00:11:26 -0400

    yelpcamp and restful project added

--- assets/js/kulArtist.js
+++ assets/js/kulArtist.js
@@ -2,6 +2,24 @@
 var projectDetails=[
 {
 	id:"0",
+	name:"Yelp Camp",
+	heading:"Website | Javascript, NodeJs, ExpressJs, MongoDb, AWS Cloud9, NPM, Mongoose",
+	description:"Responsive web design to display list of campgrounds to the users <br>User authentication (Login/Register/Logout) using passport tool <br> User authorization for adding/updating/deleting campground or comments <br>Restful routing according to REST standards <br>AWS Cloud9 IDE along with MongoDb for development and data storage <br>Backend implementation with expressJs, NodeJs and mongoose.",
+	demo:"https://gilpix.github.io/My-Portfolio/",
+	code:"https://github.com/Gilpix/YelpCamp",
+	image:"yelpcamp.png"
+ },
+ {
+ 	id:"1",
+	name:"RESTful Blog",
+	heading:"Website : Javascript, NodeJs, ExpressJs, Sementic UI, MongoDb, AWS Cloud9",
+	description:"A RESTful routes blog for creating, reading, updating and deleting blog post <br>Responsive web design using semantic ui <br>AWS Cloud9 IDE and MongoDb for development and data storage. ",
+	demo:"https://gilpix.github.io/My-Portfolio/",
+	code:"https://github.com/Gilpix/RESTFulBlog",
+	image:"RestFul Blog.png"
+ },
+{
+	id:"2",
 	name:"My Portfolio",
 	heading:"Kuldeep Singh Software Developer Portfolio Website",
 	description:"KulArtist is an software developer portfolio static website for me(Kuldeep Singh). Designed and written in html, css and javascript. Used two famous librearies <strong>Bootstrap  4.0</strong> and <strong>jQuery</strong> for more effects and designing.",
@@ -10,7 +28,7 @@ var projectDetails=[
 	image:"myportfolio.png"
  },
  {
-	id:"1",
+	id:"3",
 	name:"Tekhub",
 	heading:"Website : TekHub | Html, Css, Javascript, jQuery, Bootstrap 4",
 	description:"It is a front-end design of product lending system  used by the students of CEGEP de la gaspesie to borrow devices  like laptops, lan cables, etc. from the college.<br>Responsive web design for students to borrow item from college <br> Forms(signIn, signUp) validation <br> jQuery and bootstrap libraries included for better design and effects. ",
@@ -19,17 +37,16 @@ var projectDetails=[
 	image:"tekhubweb.png"
  },
  {
-	id:"2",
+	id:"4",
 	name:"Tekhub",
 	heading:"Android Lending App : TekHub | Android, Java, WebApi, AmazonRDS MySql",
 	description:"TekHub application is  used by the students of CEGEP de la gaspesie to borrow devices  like laptops, lan cables, etc. from the college.<br>Achieved login validation, registration of user and admin<br>Web calls for data exchange between app and AmazonRDS Mysql database<br>Database connectivity between AmazonRDS - Netbeans(webcall) - Android<br>Added features to add item in waiting list, borrow items, filter items list, give feedback/ratings and updatethe database accordingly.<br>Used Agile methodology-SCRUM to develop the app and created SRS documentation. <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/LendSystemIteration3.pdf' download>SRS-Document</a>  |  <a class='modal-res-links' target='_blank' href='assets/res/TekhubFinalPresentation.pdf' download>Presentation</a>  |  <a class='modal-res-links' target='_blank' href='https://github.com/Gilpix/TekHub-WebCalls' download>Web-Api</a>",
-	
 	demo:"https://github.com/Gilpix/TekHub",
 	code:"https://github.com/Gilpix/TekHub",
 	image:"tekhubmobile.png"
  },
  {
-	id:"3",
+	id:"5",
 	name:"Food Buddy",
 	heading:"Android App : FoodBuddy | Android, Java, Firebase, Xampp, MySql",
 	description:"The FoodBuddy application will provide its user the ability to select a restaurant and search for a buddy/partner who can join him/her for food.<br>Developed new user registration along with reset password functionality.<br> Displayed list of available users in recycler view from mysql db using xampp server and phpMyadmin web application. <br>Send invitation(notification) to other user using Firebase Cloud Messaging(FCM) service. <br> Developed SRS document for app development. <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/foodbuddySrs.pdf' download>SRS-Document</a>",
@@ -38,7 +55,7 @@ var projectDetails=[
 	image:"foodbuddy.png"
  },
  {
-	id:"4",
+	id:"6",
 	name:"Car Market",
 	heading:"Website design - html | css",
 	description:"Car Market is a front-end design of car selling web application.<br>Fully designed website includes home page, contact page, signIn/signUp page and car display page.",
@@ -47,7 +64,7 @@ var projectDetails=[
 	image:"carmarket.png"
  },
  {
-	id:"5",
+	id:"7",
 	name:"Traffic Road Sign Canada",
 	heading:"Android Traffic Road Sign Canada App | Android, Google Play+Console",
 	description:"It is a theory and quiz based android app for users to prepare for Canadian driving test. <br> Uploaded on Google play store for better understanding of the insights of app and users using Google developer play console. ",
@@ -56,7 +73,7 @@ var projectDetails=[
 	image:"traffic.png"
  },
  {
-	id:"6",
+	id:"8",
 	name:"Color Guessing Game",
 	heading:"Web Application | javascript, css, bootstrap4, html",
 	description:"Web application using Javascript CSS HTML - Wher user can select any color square from the given squares depending on the RGB color value shown. If user guesses right color square, user wins else the selected square will dissappear and now user can guess from the remaining squares. Ther will be two mode easy mode with just there squares and hard mode with six squares.",
@@ -64,7 +81,7 @@ var projectDetails=[
 	code:"https://github.com/Gilpix/ColorGuessingGame",
 	image:"colorguess.png"
  },{
-	id:"7",
+	id:"9",
 	name:"To Do List",
 	heading:"Web Application | jQuery, CSS, HTML",
 	description:"Resposive web application ToDoList using jQuery, CSS and HTML, where user can add item to list, remove item from list or mark completed items.",
@@ -73,7 +90,7 @@ var projectDetails=[
 	image:"todolist.png"
  },
  {
-	id:"8",
+	id:"10",
 	name:"Travel With Us",
 	heading:"Android App Design | Prototype : JustInmind software",
 	description:"Travel With Us is an android application screens design and prototype for bus booking system.<br>It is benificial for two type of travellers Intercity and Intracity. User can easily book tickets and can also cancel them if needed.<br>Intercity Bus Booking : In this category user can use this app to travel within the city.<br>Intracity Bus Booking : In this category user can use this app to travel from one city to another.   <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/travelWithUs.docx' download>Screen-Flow</a>",
@@ -82,7 +99,7 @@ var projectDetails=[
 	image:"travel.png"
  },
  {
-	id:"9",
+	id:"11",
 	name:"Share Expenses",
 	heading:"Android App Design | Prototype : Justinmind software",
 	description:"Share Expenses is an android application design which is used to calculate expenses for single user and split expenses between two or more users.<br><strong>Features :</strong><br>Calculate weekly/monthly/yearly expenses of single user<br>Split expenses between users<br>Remind other users to clear out their dues<br>Receive notification for any updation/creation/deletion of bill<br>Add new friends or create new groups easily<br>Can add bills when offline  <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/Share Expense ScreenFlowDiagram.pdf' download>Screen-Flow</a>  |  <a class='modal-res-links' target='_blank' href='assets/res/ShareExpensesPresentation.pdf' download>Presentation</a>",
@@ -91,7 +108,7 @@ var projectDetails=[
 	image:"shareexpenses.png"
  },
  {
-	id:"10",
+	id:"12",
 	name:"Pro Quiz",
 	heading:"Android Quiz application | Android programming, xml, google play store",
 	description:"Pro Quiz is a free game to let you enjoy guessing lots of question, picture and audio from different categories like sports, logos, music, nature, monuments etc. Pro Quiz is a simple game based on the users general knowledge and on the basis of user knowledge, user is divided into for groups Poor, Average, Good and Excellent.",
@@ -126,12 +143,12 @@ function modal(id,name,shortDesc, description,demo,code,image){
 	$(".project-demo").attr("href",demo);
 	$(".project-code").attr("href",code);
 	$(".modal-image").attr("src","assets/res/"+image);
-	if(id==9 || id==8)
+	if(id==11 || id==10)
 	{
 		$(".project-code").addClass("hide-cards");
 		$(".project-demo").addClass("hide-cards");
 	}
-	else if(id==2 || id==3)
+	else if(id==4 || id==5 || id==0 || id==1)
 	{
 		$(".project-demo").addClass("hide-cards");
 	}
@@ -149,6 +166,22 @@ var projectInfo=[
 
 	{
 		id:"0",
+		title:"Yelp Camp",
+		languages:"mongoDb/expressJs/nodeJs",
+		category:"web",
+		icon:"<i class='fa fa-2x fa-campground'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"1",
+		title:"RESTful Blog",
+		languages:"mongoDb/expressJs/nodeJs",
+		category:"web",
+		icon:"<i class='fab fa-2x fa-blogger'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"2",
 		title:"My Portfolio",
 		languages:"html/css/jQuery",
 		category:"web",
@@ -156,7 +189,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"1",
+		id:"3",
 		title:"TekHub",
 		languages:"html/css/javascript",
 		category:"web",
@@ -164,7 +197,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"2",
+		id:"4",
 		title:"TekHub",
 		languages:"java/android/mysql",
 		category:"mobile",
@@ -172,7 +205,7 @@ var projectInfo=[
 		cardImage:"card-img-mobile"
 	},
 	{
-		id:"3",
+		id:"5",
 		title:"Food Buddy",
 		languages:"android/xml/mysql",
 		category:"mobile",
@@ -181,7 +214,7 @@ var projectInfo=[
 	},
 	
 	{
-		id:"4",
+		id:"6",
 		title:"Car Market",
 		languages:"html/css",
 		category:"web",
@@ -189,7 +222,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"5",
+		id:"7",
 		title:"Traffic Road Sign",
 		languages:"java/android/xml",
 		category:"mobile",
@@ -197,7 +230,7 @@ var projectInfo=[
 		cardImage:"card-img-mobile"
 	},
 	{
-		id:"6",
+		id:"8",
 		title:"Color Guess",
 		languages:"html/css/javascript",
 		category:"web",
@@ -205,7 +238,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"7",
+		id:"9",
 		title:"ToDo List",
 		languages:"jQuery/css/html",
 		category:"web",
@@ -213,7 +246,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"8",
+		id:"10",
 		title:"Travel with us",
 		languages:"justinmind/design/prototype",
 		category:"other",
@@ -221,7 +254,7 @@ var projectInfo=[
 		cardImage:"card-img-others"
 	},
 	{
-		id:"9",
+		id:"11",
 		title:"Share Expenses",
 		languages:"justinmind/ui design",
 		category:"other",
@@ -229,7 +262,7 @@ var projectInfo=[
 		cardImage:"card-img-others"
 	},
 	{
-		id:"10",
+		id:"12",
 		title:"Pro Quiz",
 		languages:"java/android/play-store",
 		category:"mobile",
