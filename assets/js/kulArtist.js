536672c9545a94502b2f29cf6e83825756e450b7
Gilpix <kuldeepsingh5005@gmail.com>
2020-11-15 11:13:18 -0500

    Piknix and SaniAmigo projects added in my portfolio

--- assets/js/kulArtist.js
+++ assets/js/kulArtist.js
@@ -1,7 +1,29 @@
 //Object of Projects details for modal
+
+
+
 var projectDetails=[
+	{
+		id:"0",
+		name:"Piknix",
+		heading:"Software Developer (full time) <br> Javascript, NodeJs, ExpressJs,HTML5, MySql, Bootstrap 4, Sequelize, Passport, Nginx, Visual Code, Digital Ocean, Docker, GoDaddy",
+		description:"Piknix is an e-commerce marketplace for shopping local groceries online. It allows local merchants to register and sell their products. I am working as a software developer with a team of 2.<br>-> Contributed in all phases of the development lifecycle <br>-> Designed and developed views/templates on top of javascript, using the ejs template engine <br>-> Web page development from design to test and up to production deployment. <br>-> Worked on design(database schema) and implementation of various REST API’s using ice factory pattern <br>-> Created and implemented a custom CSS grid system, with CSS media queries for mobile responsiveness. <br>-> Created HTML email templates and newsletters for e-mail marketing campaigns.<br>-> Integrated PayPal payment gateway, google API’s, passport authentication and authorization, i18n localization, send grid email API, Twilio SMS API <br>-> Worked with Express MVC framework for NodeJS to create the routes required for delivering data to the front end. <br>-> Created docker image for configuration simplifications <br>-> Server setup using nginx as web server and added SSL certificates <br>-> Building service components and complex business logic <br>-> Used MySQL for database and Sequelize ORM <br>-> Did on site SEO implementations to increase traffic to client websites <br>-> Analyzed traffic and made necessary changes for a better user experience",
+		demo:"https://www.piknix.com",
+		code:"https://www.piknix.com",
+		image:"piknix.png"
+	 },
+	 {
+		 id:"1",
+		name:"Sani Amigo",
+		heading:"Freelance Project from 4S Innovative Solutions Inc. <br> Javascript, HTML5, Bootstrap 4, NodeJs, Express, Nginx, VisualCode, Docker, GoDaddy",
+		description:"saniamigo.com is a promotional website for a smart personal device that dispenses sanitizer named Sani-Amigo by 4S Innovative Solutions Inc. <br>- Design and developed responsive application using mobile rst design approach <br>- Created HTML email templates and newsletters for e-mail marketing campaigns <br>- Server setup using nginx as webserver and added Let's Encrypt SSL certificates <br>- Web page development from design to test and up to production deployment.",
+		demo:"https://saniamigo.com/",
+		code:"https://saniamigo.com/",
+		image:"saniamigo.png"
+	 },
+
 {
-	id:"0",
+	id:"2",
 	name:"Yelp Camp",
 	heading:"Website | Javascript, NodeJs, ExpressJs, MongoDb, AWS Cloud9, NPM, Mongoose",
 	description:"Responsive web design to display list of campgrounds to the users <br>User authentication (Login/Register/Logout) using passport tool <br> User authorization for adding/updating/deleting campground or comments <br>Restful routing according to REST standards <br>AWS Cloud9 IDE along with MongoDb for development and data storage <br>Backend implementation with expressJs, NodeJs and mongoose.",
@@ -10,7 +32,7 @@ var projectDetails=[
 	image:"yelpcamp.png"
  },
  {
- 	id:"1",
+ 	id:"3",
 	name:"RESTful Blog",
 	heading:"Website : Javascript, NodeJs, ExpressJs, Sementic UI, MongoDb, AWS Cloud9",
 	description:"A RESTful routes blog for creating, reading, updating and deleting blog post <br>Responsive web design using semantic ui <br>AWS Cloud9 IDE and MongoDb for development and data storage. ",
@@ -19,7 +41,7 @@ var projectDetails=[
 	image:"RestFul Blog.png"
  },
 {
-	id:"2",
+	id:"4",
 	name:"My Portfolio",
 	heading:"Kuldeep Singh Software Developer Portfolio Website",
 	description:"KulArtist is an software developer portfolio static website for me(Kuldeep Singh). Designed and written in html, css and javascript. Used two famous librearies <strong>Bootstrap  4.0</strong> and <strong>jQuery</strong> for more effects and designing.",
@@ -28,7 +50,7 @@ var projectDetails=[
 	image:"myportfolio.png"
  },
  {
-	id:"3",
+	id:"5",
 	name:"Tekhub",
 	heading:"Website : TekHub | Html, Css, Javascript, jQuery, Bootstrap 4",
 	description:"It is a front-end design of product lending system  used by the students of CEGEP de la gaspesie to borrow devices  like laptops, lan cables, etc. from the college.<br>Responsive web design for students to borrow item from college <br> Forms(signIn, signUp) validation <br> jQuery and bootstrap libraries included for better design and effects. ",
@@ -37,7 +59,7 @@ var projectDetails=[
 	image:"tekhubweb.png"
  },
  {
-	id:"4",
+	id:"6",
 	name:"Tekhub",
 	heading:"Android Lending App : TekHub | Android, Java, WebApi, AmazonRDS MySql",
 	description:"TekHub application is  used by the students of CEGEP de la gaspesie to borrow devices  like laptops, lan cables, etc. from the college.<br>Achieved login validation, registration of user and admin<br>Web calls for data exchange between app and AmazonRDS Mysql database<br>Database connectivity between AmazonRDS - Netbeans(webcall) - Android<br>Added features to add item in waiting list, borrow items, filter items list, give feedback/ratings and updatethe database accordingly.<br>Used Agile methodology-SCRUM to develop the app and created SRS documentation. <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/LendSystemIteration3.pdf' download>SRS-Document</a>  |  <a class='modal-res-links' target='_blank' href='assets/res/TekhubFinalPresentation.pdf' download>Presentation</a>  |  <a class='modal-res-links' target='_blank' href='https://github.com/Gilpix/TekHub-WebCalls' download>Web-Api</a>",
@@ -46,7 +68,7 @@ var projectDetails=[
 	image:"tekhubmobile.png"
  },
  {
-	id:"5",
+	id:"7",
 	name:"Food Buddy",
 	heading:"Android App : FoodBuddy | Android, Java, Firebase, Xampp, MySql",
 	description:"The FoodBuddy application will provide its user the ability to select a restaurant and search for a buddy/partner who can join him/her for food.<br>Developed new user registration along with reset password functionality.<br> Displayed list of available users in recycler view from mysql db using xampp server and phpMyadmin web application. <br>Send invitation(notification) to other user using Firebase Cloud Messaging(FCM) service. <br> Developed SRS document for app development. <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/foodbuddySrs.pdf' download>SRS-Document</a>",
@@ -55,7 +77,7 @@ var projectDetails=[
 	image:"foodbuddy.png"
  },
  {
-	id:"6",
+	id:"8",
 	name:"Car Market",
 	heading:"Website design - html | css",
 	description:"Car Market is a front-end design of car selling web application.<br>Fully designed website includes home page, contact page, signIn/signUp page and car display page.",
@@ -64,7 +86,7 @@ var projectDetails=[
 	image:"carmarket.png"
  },
  {
-	id:"7",
+	id:"9",
 	name:"Traffic Road Sign Canada",
 	heading:"Android Traffic Road Sign Canada App | Android, Google Play+Console",
 	description:"It is a theory and quiz based android app for users to prepare for Canadian driving test. <br> Uploaded on Google play store for better understanding of the insights of app and users using Google developer play console. ",
@@ -73,7 +95,7 @@ var projectDetails=[
 	image:"traffic.png"
  },
  {
-	id:"8",
+	id:"10",
 	name:"Color Guessing Game",
 	heading:"Web Application | javascript, css, bootstrap4, html",
 	description:"Web application using Javascript CSS HTML - Wher user can select any color square from the given squares depending on the RGB color value shown. If user guesses right color square, user wins else the selected square will dissappear and now user can guess from the remaining squares. Ther will be two mode easy mode with just there squares and hard mode with six squares.",
@@ -81,7 +103,7 @@ var projectDetails=[
 	code:"https://github.com/Gilpix/ColorGuessingGame",
 	image:"colorguess.png"
  },{
-	id:"9",
+	id:"11",
 	name:"To Do List",
 	heading:"Web Application | jQuery, CSS, HTML",
 	description:"Resposive web application ToDoList using jQuery, CSS and HTML, where user can add item to list, remove item from list or mark completed items.",
@@ -90,7 +112,7 @@ var projectDetails=[
 	image:"todolist.png"
  },
  {
-	id:"10",
+	id:"12",
 	name:"Travel With Us",
 	heading:"Android App Design | Prototype : JustInmind software",
 	description:"Travel With Us is an android application screens design and prototype for bus booking system.<br>It is benificial for two type of travellers Intercity and Intracity. User can easily book tickets and can also cancel them if needed.<br>Intercity Bus Booking : In this category user can use this app to travel within the city.<br>Intracity Bus Booking : In this category user can use this app to travel from one city to another.   <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/travelWithUs.docx' download>Screen-Flow</a>",
@@ -99,7 +121,7 @@ var projectDetails=[
 	image:"travel.png"
  },
  {
-	id:"11",
+	id:"13",
 	name:"Share Expenses",
 	heading:"Android App Design | Prototype : Justinmind software",
 	description:"Share Expenses is an android application design which is used to calculate expenses for single user and split expenses between two or more users.<br><strong>Features :</strong><br>Calculate weekly/monthly/yearly expenses of single user<br>Split expenses between users<br>Remind other users to clear out their dues<br>Receive notification for any updation/creation/deletion of bill<br>Add new friends or create new groups easily<br>Can add bills when offline  <br><br><strong>Check</strong> : <a class='modal-res-links' target='_blank' href='assets/res/Share Expense ScreenFlowDiagram.pdf' download>Screen-Flow</a>  |  <a class='modal-res-links' target='_blank' href='assets/res/ShareExpensesPresentation.pdf' download>Presentation</a>",
@@ -108,7 +130,7 @@ var projectDetails=[
 	image:"shareexpenses.png"
  },
  {
-	id:"12",
+	id:"14",
 	name:"Pro Quiz",
 	heading:"Android Quiz application | Android programming, xml, google play store",
 	description:"Pro Quiz is a free game to let you enjoy guessing lots of question, picture and audio from different categories like sports, logos, music, nature, monuments etc. Pro Quiz is a simple game based on the users general knowledge and on the basis of user knowledge, user is divided into for groups Poor, Average, Good and Excellent.",
@@ -138,19 +160,23 @@ function showModal(no){
 //Show Modal for selected project
 function modal(id,name,shortDesc, description,demo,code,image){
 	$(".modal-heading").text(name);
-	$(".modal-short-text").text(shortDesc);
+	$(".modal-short-text").html(shortDesc);
 	$(".modal-desc-text").html(description);
 	$(".project-demo").attr("href",demo);
 	$(".project-code").attr("href",code);
 	$(".modal-image").attr("src","assets/res/"+image);
-	if(id==11 || id==10)
+	if(id==13 || id==12)
 	{
 		$(".project-code").addClass("hide-cards");
 		$(".project-demo").addClass("hide-cards");
 	}
-	else if(id==4 || id==5 || id==0 || id==1)
+	else if(id==6 || id==7 || id==2 || id==3)
 	{
 		$(".project-demo").addClass("hide-cards");
+	}
+	else if(id==0 || id==1)
+	{
+		$(".project-code").addClass("hide-cards");
 	}
 	else
 	{
@@ -166,6 +192,23 @@ var projectInfo=[
 
 	{
 		id:"0",
+		title:"Piknix",
+		languages:"Software Developer at Piknix",
+		category:"web",
+		icon:"<i class='fas fa-2x fa-shopping-bag'></i><br>",
+		cardImage:"card-img-web"
+	},
+	{
+		id:"1",
+		title:"Sani Amigo",
+		languages:"Project by 4S Innovative Solutions Inc.",
+		category:"web",
+		icon:"<i class='fas fa-2x fa-wine-bottle'></i><br>",
+		cardImage:"card-img-web"
+	},
+
+	{
+		id:"2",
 		title:"Yelp Camp",
 		languages:"mongoDb/expressJs/nodeJs",
 		category:"web",
@@ -173,7 +216,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"1",
+		id:"3",
 		title:"RESTful Blog",
 		languages:"mongoDb/expressJs/nodeJs",
 		category:"web",
@@ -181,7 +224,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"2",
+		id:"4",
 		title:"My Portfolio",
 		languages:"html/css/jQuery",
 		category:"web",
@@ -189,7 +232,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"3",
+		id:"5",
 		title:"TekHub",
 		languages:"html/css/javascript",
 		category:"web",
@@ -197,7 +240,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"4",
+		id:"6",
 		title:"TekHub",
 		languages:"java/android/mysql",
 		category:"mobile",
@@ -205,7 +248,7 @@ var projectInfo=[
 		cardImage:"card-img-mobile"
 	},
 	{
-		id:"5",
+		id:"7",
 		title:"Food Buddy",
 		languages:"android/xml/mysql",
 		category:"mobile",
@@ -214,7 +257,7 @@ var projectInfo=[
 	},
 	
 	{
-		id:"6",
+		id:"8",
 		title:"Car Market",
 		languages:"html/css",
 		category:"web",
@@ -222,7 +265,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"7",
+		id:"9",
 		title:"Traffic Road Sign",
 		languages:"java/android/xml",
 		category:"mobile",
@@ -230,7 +273,7 @@ var projectInfo=[
 		cardImage:"card-img-mobile"
 	},
 	{
-		id:"8",
+		id:"10",
 		title:"Color Guess",
 		languages:"html/css/javascript",
 		category:"web",
@@ -238,7 +281,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"9",
+		id:"11",
 		title:"ToDo List",
 		languages:"jQuery/css/html",
 		category:"web",
@@ -246,7 +289,7 @@ var projectInfo=[
 		cardImage:"card-img-web"
 	},
 	{
-		id:"10",
+		id:"12",
 		title:"Travel with us",
 		languages:"justinmind/design/prototype",
 		category:"other",
@@ -254,7 +297,7 @@ var projectInfo=[
 		cardImage:"card-img-others"
 	},
 	{
-		id:"11",
+		id:"13",
 		title:"Share Expenses",
 		languages:"justinmind/ui design",
 		category:"other",
@@ -262,7 +305,7 @@ var projectInfo=[
 		cardImage:"card-img-others"
 	},
 	{
-		id:"12",
+		id:"14",
 		title:"Pro Quiz",
 		languages:"java/android/play-store",
 		category:"mobile",
